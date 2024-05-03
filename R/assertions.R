b990ef926af9e2c641ed09566ac84253a5fdae47
Kennedy Mwavu <mwavukennedy@gmail.com>
2024-05-03 17:35:21 +0300

    ch: assert that project type is valid

--- R/assertions.R
+++ R/assertions.R
@@ -5,3 +5,11 @@ not_missing <- function(x){
 assertthat::on_failure(not_missing) <- function(call, env){
   paste("Missing", deparse(call$x))
 }
+
+is_valid_project_type <- function(type) {
+  type %in% c("backend", "frontend")
+}
+
+assertthat::on_failure(is_valid_project_type) <- function(call, env) {
+  "Invalid project type. Must be one of 'frontend' or 'backend'."
+}
